05dccfe09f4c4d6d3b72be78260b177f9beb09de
Viviane Ferreira <vferreiradeoliveira595@gmail.com>
2021-11-12 19:40:43 -0300

    Minificando arquivos..

--- gulpFile.js
+++ gulpFile.js
@@ -6,9 +6,20 @@ const uglifycss = require('gulp-uglifycss')
 
 function minifyCSS() {
     return gulp.src('./assets/css/style.css')
-        .pipe(uglifycss({ "uglyComments" : true }))
-        .pipe(concat('estilo.min.css'))
-        .pipe(gulp.dest('./assets/css'))
+            .pipe(uglifycss({ "uglyComments" : true }))
+            .pipe(concat('estilo.min.css'))
+            .pipe(gulp.dest('./build/assets/css'))
 }
 
-exports.default = series(minifyCSS)
+function copyHTML() {
+    return gulp.src('index.html')
+            .pipe(uglifycss({ "uglyComments" : true }))
+            .pipe(gulp.dest('./build/'))
+}
+
+function copyIMG() {
+    return gulp.src('./assets/img/**/*.*')
+            .pipe(gulp.dest('build/assets/img'))
+}
+
+exports.default = series(minifyCSS, copyHTML, copyIMG)
